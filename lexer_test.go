a6661cbd87588689e3787da844bdd53a3c2d91d3
Federico Ficarelli <federico.ficarelli@gmail.com>
2019-11-09 18:07:17 +0100

    Add keyword tests

--- lexer_test.go
+++ lexer_test.go
@@ -22,6 +22,23 @@ var lextests = []struct {
 	{"BlockComment4", "(*else(*then(*if*)*)class(*(*loop*)pool*)case*)", nil},
 	{"Whitespace", "    \t\t \f \v \r\r\r\n\n      ", nil},
 	{"BoolLiterals", "true false tRUE fALSE True False", []Token{BOOLLITERAL, BOOLLITERAL, BOOLLITERAL, BOOLLITERAL, TYPEID, TYPEID}},
+	{"KeywordClass", "class CLASS Class cLASS", []Token{CLASS, CLASS, CLASS, CLASS}},
+	{"KeywordElse", "else ELSE Else eLSE", []Token{ELSE, ELSE, ELSE, ELSE}},
+	{"KeywordIf", "if IF If iF", []Token{IF, IF, IF, IF}},
+	{"KeywordFi", "fi FI Fi fI", []Token{FI, FI, FI, FI}},
+	{"KeywordIn", "in IN In iN", []Token{IN, IN, IN, IN}},
+	{"KeywordInherits", "inherits INHERITS Inherits iNHERITS", []Token{INHERITS, INHERITS, INHERITS, INHERITS}},
+	{"KeywordIsvoid", "isvoid ISVOID Isvoid iSVOID", []Token{ISVOID, ISVOID, ISVOID, ISVOID}},
+	{"KeywordLet", "let LET Let lET", []Token{LET, LET, LET, LET}},
+	{"KeywordLoop", "loop LOOP Loop lOOP", []Token{LOOP, LOOP, LOOP, LOOP}},
+	{"KeywordPool", "pool POOL Pool pOOL", []Token{POOL, POOL, POOL, POOL}},
+	{"KeywordThen", "then THEN Then tHEN", []Token{THEN, THEN, THEN, THEN}},
+	{"KeywordWhile", "while WHILE While wHILE", []Token{WHILE, WHILE, WHILE, WHILE}},
+	{"KeywordCase", "case CASE Case cASE", []Token{CASE, CASE, CASE, CASE}},
+	{"KeywordEsac", "esac ESAC Esac eSAC", []Token{ESAC, ESAC, ESAC, ESAC}},
+	{"KeywordNew", "new NEW New nEW", []Token{NEW, NEW, NEW, NEW}},
+	{"KeywordOf", "of OF Of oF", []Token{OF, OF, OF, OF}},
+	{"KeywordNot", "not NOT Not nOT", []Token{NOT, NOT, NOT, NOT}},
 }
 
 func scan(source string) []Token {

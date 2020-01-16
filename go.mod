618160bd19c3ebdc3dc1c1ac0640e0aabedb1920
Federico Ficarelli <federico.ficarelli@gmail.com>
2020-01-16 17:59:35 +0100

    Add tooling deps in go.mod
    This approach is considered the current best practice: https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/nazavode/cool
 
 go 1.13
 
-require github.com/google/go-cmp v0.3.1
+require (
+	github.com/google/go-cmp v0.3.1
+	github.com/inspirer/textmapper v0.0.0-20200106195440-4d45f61c212e
+)
---
+++ tools.go
@@ -0,0 +1,10 @@
+// +build tools
+
+package tools
+
+// Tooling dependencies, as described here:
+// https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
+
+import (
+	_ "github.com/inspirer/textmapper/tm-go/cmd/textmapper"
+)
